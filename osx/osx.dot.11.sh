a9deac35b7360e40edef6b6324d72780372a2fe1
Matthew Duck <matt@mattduck.com>
2013-11-06 21:26:37 +0000

    Mac bash completion and finder functions

--- osx/osx.dot.11.sh
+++ osx/osx.dot.11.sh
@@ -1,5 +1,10 @@
 if [[ "$(uname -a)" != *Darwin* ]]; then return; fi
 
+# Bash completion lives in the brew directory
+if [ -f $(brew --prefix)/etc/bash_completion ]; then
+  . $(brew --prefix)/etc/bash_completion
+fi
+
 # Use coreutils to support solarized colours
 alias ls="gls --color=auto"
 alias dircolors="gdircolors"
@@ -7,7 +12,7 @@ alias sort="gsort"
 
 alias emacs-app="/Applications/Emacs.app/Contents/MacOS/Emacs"
 
-function ,cdfinder() {
+function ,finder-pull() {
     # cd to topmost Finder window directory
     cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')"
 }
