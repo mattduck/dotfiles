b6aa5f5ed4339452b32299351c9926c342e3f695
Matthew Duck <matt@mattduck.com>
2013-11-06 13:51:59 +0000

    Rename osx shell file

--- osx/osx.dot.11.sh
+++ osx/osx.dot.11.sh
@@ -6,3 +6,8 @@ alias dircolors="gdircolors"
 alias sort="gsort"
 
 alias emacs-app="/Applications/Emacs.app/Contents/MacOS/Emacs"
+
+function ,cdfinder() {
+    # cd to topmost Finder window directory
+    cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')"
+}
