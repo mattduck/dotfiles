89b2ae84fe80a1086be18014051ab88c5f08bcd2
Matthew Duck <matt@mattduck.com>
2013-11-06 13:50:02 +0000

    More aliases

--- bash/aliases.dot.10.sh
+++ bash/aliases.dot.10.sh
@@ -1,5 +1,14 @@
+alias sudo="sudo " # Enable aliases to be sudo'ed
+
 alias la="ls -A"
 alias l="ls -CF"
 alias ll="ls -alF"
+
 alias v="vim"
 alias h="history"
+alias g="grep"
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias ~~="cd ~"
