4c614412a8bf32edf1580c06b3d702c890400899
Matthew Duck <matt@mattduck.com>
2013-11-07 10:23:39 +0000

    Colour aliases

--- bash/aliases.dot.10.sh
+++ bash/aliases.dot.10.sh
@@ -1,5 +1,10 @@
 alias sudo="sudo " # Enable aliases to be sudo'ed
 
+alias ls="ls --color=auto"
+alias grep="grep --color=auto"
+alias fgrep="fgrep --color=auto"
+alias egrep="egrep --color=auto"
+
 alias la="ls -A"
 alias l="ls -CF"
 alias ll="ls -alF"
