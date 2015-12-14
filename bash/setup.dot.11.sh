d9e9e7f72e058be671036d2dab52a5115b800bf0
Matthew Duck <matt@mattduck.com>
2015-12-14 15:20:01 +0000

    Set PYTHONDONTWRITEBYTECODE

--- bash/setup.dot.11.sh
+++ bash/setup.dot.11.sh
@@ -8,6 +8,11 @@ shopt -s histappend
 
 ,path-add --prepend $HOME/bin
 
+# Create neither *.pyc files nor __pycache__ directories.
+export PYTHONDONTWRITEBYTECODE=1
+
+# By default, Python's virtualenv will modify the prompt when a virtualenv is
+# active. Disable this.
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 
 export GREP_COLOR="1;4;31"
