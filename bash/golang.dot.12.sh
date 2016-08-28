8b6449d787fb5a3c88ad9e68bd870397ed158f92
Matthew Duck <matt@mattduck.com>
2016-08-28 21:21:56 +0100

    Add go aliases for cross compiling

--- bash/golang.dot.12.sh
+++ bash/golang.dot.12.sh
@@ -4,3 +4,6 @@ mkdir -p "$GOPATH"
 
 # Generally I prob won't use <= 1.7 now, but I'll keep this just in case.
 export GO15VENDOREXPERIMENT=1
+
+alias golinux="GOOS=linux GOARCH=amd64 go"
+alias godarwin="GOOS=darwin GOARCH=amd64 go"
