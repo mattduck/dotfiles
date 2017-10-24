607bd0e4a7552e2928d1f126df9f3b5f32c62dc9
Matt Duck <matt@ometria.com>
2017-10-24 21:46:06 +0100

    Add fab completion

--- bash/python.dot.17.sh
+++ bash/python.dot.17.sh
@@ -1,6 +1,11 @@
 # Create neither *.pyc files nor __pycache__ directories.
 export PYTHONDONTWRITEBYTECODE=1
 
+# Automatic fab completion
+# shellcheck source=/dev/null
+. "$DOTFILES/bash/fabric-completion/fabric-completion.bash"
+export FAB_COMPLETION_CACHE_TASKS=false
+
 # By default, Python's virtualenv will modify the prompt when a virtualenv is
 # active. Disable this.
 export VIRTUAL_ENV_DISABLE_PROMPT=1
