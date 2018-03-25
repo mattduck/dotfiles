b287bc8128a4b6227708d737602f07574a3842eb
Matthew Duck <matt@mattduck.com>
2018-03-25 13:34:20 +0100

    Emacs - rename file/buffer

--- emacs.d.symlink/init.el
+++ emacs.d.symlink/init.el
@@ -311,6 +311,18 @@
           (message "Deleted file %s" filename)
           (kill-buffer))))))
 
+(defun md/rename-file-and-buffer ()
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (message "Buffer is not visiting a file!")
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+         ((vc-backend filename) (vc-rename-file filename new-name))
+         (t
+          (rename-file filename new-name t)
+          (set-visited-file-name new-name t t)))))))
+
 (defun md/expand-newlines ()
   (interactive)
   (funcall-interactively 'replace-string "\\n" "
@@ -519,7 +531,8 @@
         ("bh" . previous-buffer)
         ("bl" . next-buffer)
         ("k" . kill-buffer)
-        ("K" . md/remove-file-and-buffer)
+        ("bK" . md/remove-file-and-buffer)
+        ("bR" . md/rename-file-and-buffer)
         ("bk" . kill-buffer)
         ("bi" . md/file-info)
         ("bw" . save-buffer)
