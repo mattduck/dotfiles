426a30b29bc8f68a8d884b1d76f101ec76dbd479
Matt Duck <matt@ometria.com>
2018-07-21 14:48:30 +0100

    Remove old tmux config

--- tmux/tmux_env.dot.sh
+++ tmux/tmux_env.dot.sh
@@ -6,48 +6,11 @@ echo "Setting up tmux environment..."
 
 tmux source-file $DOTFILES/splitscreen/tmux.conf
 
-function ,tm-init() {
-    # Setup session with a generic layout
-    if [[ ! -z "$CUSTOM_TMUX_INIT" ]]; then
-        echo "This session has already been initialised"
-        return 1
-    fi
-
-    ORIG_WINDOW=`tmux display-message -p "#I"`
-
-    # Panes
-    ,tm-new-win-vertical
-
-    # Usually only have one session per machine
-    tmux rename-session "main"
-
-    export CUSTOM_TMUX_INIT=true
-    echo "Generic tmux layout initiated"
-}
-
 function ,tm-reload() {
     # Reload tmux config and this file. Has to be done per pane.
     tmux source-file ~/.tmux.conf
     source $DOTFILES/tmux/tmux_env.dot.sh
     echo "Reloaded ~/.tmux.conf and $DOTFILES/tmux/tmux_env.sh"
-}
-
-function ,tm-new-win-vertical() {
-    tmux new-window
-    tmux splitw -v -p 70
-
-    tmux select-pane -t 1
-    tmux splitw -h -p 33
-    tmux select-pane -t 1
-    tmux splitw -h -p 50
-    tmux select-pane -t 4 # main bottom pane
-}
-
-function ,tm-new-win-horizontal() {
-    tmux new-window
-    tmux splitw -h -p 30
-    tmux splitw -v -p 50
-    tmux select-pane -t 1 # main left pane
 }
 
 function ,tm-move() {
