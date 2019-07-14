25ee1ef2d81962626add6cc673184914fef961cd
Matthew Duck <matt@mattduck.com>
2019-07-14 11:54:27 +0100

    i3 updates

--- config.symlink/i3-status-bash.sh
+++ config.symlink/i3-status-bash.sh
@@ -30,8 +30,8 @@ while :; do
     else ICON_AUDIO=$ICON_AUDIO_EMPTY;
     fi
 
-    BATTERY_PERCENT=$(acpi | grep -v Unknown | awk '{print $4}' | sed 's/,//' | sed 's/%//')
-    BATTERY_TIME_REMAINING=$(acpi | grep -v Unknown | awk '{print $5}' | sed 's/,//' | cut -d ":" -f 1,2)
+    BATTERY_PERCENT=$(acpi | grep 'Battery 1' | awk '{print $4}' | sed 's/,//' | sed 's/%//')
+    BATTERY_TIME_REMAINING=$(acpi | grep 'Battery 1' | awk '{print $5}' | sed 's/,//' | cut -d ":" -f 1,2)
     if [[ $BATTERY_PERCENT -gt 80 ]]; then ICON_BATTERY=$ICON_BATTERY_FULL;
     elif [[ $BATTERY_PERCENT -gt 60 ]]; then ICON_BATTERY=$ICON_BATTERY_THREE_QUARTERS;
     elif [[ $BATTERY_PERCENT -gt 30 ]]; then ICON_BATTERY=$ICON_BATTERY_HALF;
@@ -41,7 +41,7 @@ while :; do
 
     NETWORK_NAME=$(iwgetid --raw)
     WIFI_SIGNAL=''
-    BATTERY_STATUS=$(acpi | grep -v Unknown | awk '{print $3}' | sed 's/,//')
+    BATTERY_STATUS=$(acpi | grep 'Battery 1' | awk '{print $3}' | sed 's/,//')
     BRIGHTNESS=$(xbacklight -get | awk -F . '{print $1}')
     ETHERNET=''
     LOAD=$(cat /proc/loadavg | awk '{ print $1 }')
@@ -52,7 +52,8 @@ while :; do
     text "     $ICON_WIFI/$NETWORK_NAME"
     text "     $ICON_AUDIO/$AUDIO" $GREY
     text "     $ICON_BRIGHTNESS/$BRIGHTNESS"
-    text "     $ICON_BATTERY/$BATTERY_PERCENT/$BATTERY_TIME_REMAINING/$BATTERY_STATUS" $GREY
+    #text "     $ICON_BATTERY/$BATTERY_PERCENT/$BATTERY_TIME_REMAINING/$BATTERY_STATUS" $GREY
+    text "     $ICON_BATTERY/$BATTERY_PERCENT/$BATTERY_STATUS" $GREY
     text "     $DATE"
     echo -e "[${output%??}],"
     sleep 3
