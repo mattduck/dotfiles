afeb30496fe9f2849b85be2358003d313edbc8b5
Matt Duck <matt@ometria.com>
2019-09-08 15:16:51 +0100

    FZF colours

--- bash/fzf.dot.16.sh
+++ bash/fzf.dot.16.sh
@@ -14,6 +14,6 @@ if [ $(command -v brew) ]; then
     source "$(fzf-prefix)/shell/completion.bash"
 fi
 
-export FZF_DEFAULT_OPTS="-0 --color=bg:8,fg:12,hl:15,bg+:0,fg+:12,hl+:15,info:6,prompt:6,pointer:1,marker:1,spinner:5,header:3 --cycle --inline-info"
+export FZF_DEFAULT_OPTS="-0 --cycle --inline-info"
 export FZF_COMPLETION_TRIGGER="*"
 export FZF_COMPLETION_OPTS='--exact'
