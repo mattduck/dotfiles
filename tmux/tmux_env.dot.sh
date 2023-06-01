bcce715d1a65c3dfd278f3c5441dfd4056a48946
Matt Duck <matt@mattduck.com>
2023-06-01 20:48:12 +0100

    chore: remove old splitscreen submodule
    This was an old separate repo where I'd configured similar
    window-managing bindings for emacs/vim/tmux, and was managing them all
    in one place. I don't want to do this now though - I'd rather just keep
    the configuration in the respective vim/emacs/tmux files rather than
    having to source this separate repo/directory just for a few lines of
    code.

--- tmux/tmux_env.dot.sh
+++ tmux/tmux_env.dot.sh
@@ -1,40 +1,9 @@
 # If this shell is running within tmux, add some extra utilities
-
 if [[ -z $TMUX ]]; then return; fi
 
-tmux source-file $DOTFILES/splitscreen/tmux.conf
-
-function ,tm-reload() {
+function ,tmux-reload() {
     # Reload tmux config and this file. Has to be done per pane.
     tmux source-file ~/.tmux.conf
     source $DOTFILES/tmux/tmux_env.dot.sh
     echo "Reloaded ~/.tmux.conf and $DOTFILES/tmux/tmux_env.sh"
-}
-
-function ,tm-move() {
-    tmux move-pane -t $1
-}
-
-function ,tm-pop() {
-    tmux break-pane
-}
-
-# Use fzf interface to swap pane
-function ,tm-goto () {
-  local panes current_window current_pane target target_window target_pane
-  panes=$(tmux list-panes -s -F '#I:#P - #W :: #{pane_current_path} :: #{pane_current_command}')
-  current_pane=$(tmux display-message -p '#I:#P')
-  current_window=$(tmux display-message -p '#I')
-
-  target=$(echo "$panes" | grep -v "$current_pane" | fzf --exact --header ",tm-goto") || return
-
-  target_window=$(echo $target | awk 'BEGIN{FS=":|-"} {print$1}')
-  target_pane=$(echo $target | awk 'BEGIN{FS=":|-"} {print$2}' | cut -c 1)
-
-  if [[ $current_window -eq $target_window ]]; then
-    tmux select-pane -t ${target_window}.${target_pane}
-  else
-    tmux select-pane -t ${target_window}.${target_pane} &&
-    tmux select-window -t $target_window
-  fi
 }
