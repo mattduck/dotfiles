2ba88e5c08ff5d91cbbafa441fa9bd97fbeb9a74
Matt Duck <matt@mattduck.com>
2023-06-01 21:35:12 +0100

    chore: pull some functions out to separate files
    Think this makes it easier to browse

--- bash/functions.dot.sh
+++ bash/functions.dot.sh
@@ -10,50 +10,14 @@ function ,cd {
     dir=$(fasd -dRl | fzf-tmux --header=",cd" --exact --query="$1" --preview="ls {}") && cd "${dir}" || return 1
 }
 
-function ,tmp {
-    if [ -t 0 ]; then
-        less /tmp/matt_tmp
-    else
-        rm /tmp/matt_tmp
-        tee /tmp/matt_tmp | less - <&0
-    fi
-}
-
-function ,github-switch-url {
-    git='git@github.com:'
-    https='https://github.com/'
-
-    url="$(git remote -v | awk '/^origin.*fetch.$/ {print $2}')"
-    echo Old url: "$url"
-
-    if echo "$url" | grep -q '^git'
-    then
-        url="${url/${git}/${https}}"
-    else
-        url="${url/${https}/${git}}"
-    fi
-
-    echo New url: "$url"
-    git remote set-url origin "$url"
-}
-
-function ,kill {
-    echo "obsolete. use fzf tab completion with kill"
-}
-
-function ,date {
-    date -u +"%Y-%m-%dT%H:%M:%S"
-}
-
 function ,env {
-    env | sort | fzf --exact
+    # Show env
+    env | sort | fzf --exact | tee /dev/tty | pbcopy
+    echo "Copied to clipboard"
 }
 
 function ,history {
     # show history without numbers
-    history | awk '{$1="";print substr($0,2)}' | fzf --exact
-}
-
-function ,k-secret-decode {
-    kubectl get secret $@ -o go-template='{{range $k,$v := .data}}{{printf "%s: " $k}}{{if not $v}}{{$v}}{{else}}{{$v | base64decode}}{{end}}{{"\n"}}{{end}}'
+    history | awk '{$1="";print substr($0,2)}' | fzf --exact | tee /dev/tty | pbcopy
+    echo "Copied to clipboard"
 }
