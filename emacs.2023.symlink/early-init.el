3fcfe8f29ba6d2fdab257368359840292955f13b
Matt Duck <matt@mattduck.com>
2024-02-28 12:38:09 +0000

    fix(emacs): use setq-default for mode line

--- emacs.2023.symlink/early-init.el
+++ emacs.2023.symlink/early-init.el
@@ -62,7 +62,7 @@ Calling this will delete the file, causing i3 to load next time."
 (md/set-default-font)
 
 ;; Want this to apply to first buffer - a very basic modeline
-(setq mode-line-format
+(setq-default mode-line-format
       `("    %b"
        ,(let ((p (and (fboundp 'project-current) (project-current))))
          (when p
